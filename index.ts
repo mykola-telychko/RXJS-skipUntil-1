e0434e68a02fd8276b6ccb55a5128f9e19109c84
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-23 22:35:11 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { interval, timer } from 'rxjs';
 import { skipUntil } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/skipuntil
+// Example 1: Skip until observable emits
 //emit every 1s
-const source = interval(1000);
+const srcInterval$ = interval(1000);
 //skip emitted values from source until inner observable emits (6s)
-const example = source.pipe(skipUntil(timer(6000)));
+const example = srcInterval$.pipe(skipUntil(timer(6000)));
 const subscribe = example.subscribe((val) => console.log(val));
 //output: 5...6...7...8...
